82283bdc6b319ab2362cb77271c2443a6d6773a8
Smolpay <Smolpay@gmail.com>
2022-09-11 17:26:25 +0300

    Домашнее задание по модулю 3 Доработка: В ProductItem один товар В ProductList в цикле собирается список

--- src/App.vue
+++ src/App.vue
@@ -54,17 +54,11 @@
 </template>
 
 <script>
-import products from '@/data/products';
-import ProductList from '@/components/ProductList';
+import ProductList from './components/ProductList.vue';
 
 export default {
   name: 'App',
   components: { ProductList },
-  data() {
-    return {
-      products,
-    };
-  },
 };
 </script>
 <style lang="stylus">
--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,54 +1,23 @@
 <template>
-  <ul class="catalog__list">
-        <li class="catalog__item" v-for = "(product, index) in products" :key="index">
-            <a class="catalog__pic" href="#">
-              <img :src="product.image" :alt="product.title">
-            </a>
-            <h3 class="catalog__title">
-              <a href="#">
-                {{ product.title }}
-              </a>
-            </h3>
-
-            <span class="catalog__price">
-              {{ product.price }}
-            </span>
-
-            <ul class="colors colors--black">
-              <li class="colors__item">
-                <label class="colors__label">
-                  <input class="colors__radio sr-only" type="radio" name="color-1"
-                         value="#73B6EA" checked="">
-                  <span class="colors__value" style="background-color: #73B6EA;">
-                  </span>
-                </label>
-              </li>
-              <li class="colors__item">
-                <label class="colors__label">
-                  <input class="colors__radio sr-only" type="radio" name="color-1" value="#8BE000">
-                  <span class="colors__value" style="background-color: #8BE000;">
-                  </span>
-                </label>
-              </li>
-              <li class="colors__item">
-                <label class="colors__label">
-                  <input class="colors__radio sr-only" type="radio" name="color-1" value="#222">
-                   <span class="colors__value" style="background-color: #222;">
-                   </span>
-                </label>
-              </li>
-            </ul>
-        </li>
+  <ul class="catalog__list" >
+        <ProductItem v-for = "(product, index) in products" :key="index" :product="product"/>
   </ul>
-
 </template>
 
 <script>
+import products from '@/data/products';
+import ProductItem from '@/components/ProductItem';
 
 export default {
   name: 'ProductList',
-  props: ['products'],
+  components: { ProductItem },
+  data() {
+    return {
+      products,
+    };
+  },
 };
+
 </script>
 
 <style scoped>
