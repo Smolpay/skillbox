5268fc8450bd57c67edafe76214826776f4f81e2
Smolpay <Smolpay@gmail.com>
2022-09-15 18:05:02 +0300

    Домашнее задание по модулю 4 Реализация действий по обучающему видео + доработка по рекомендациям линтера

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,21 +1,16 @@
 <template>
   <ul class="catalog__list" >
-        <ProductItem v-for = "(product, index) in products" :key="index" :product="product"/>
+        <ProductItem v-for = "product in products" :key="product.id" :product="product"/>
   </ul>
 </template>
 
 <script>
-import products from '@/data/products';
 import ProductItem from '@/components/ProductItem';
 
 export default {
   name: 'ProductList',
   components: { ProductItem },
-  data() {
-    return {
-      products,
-    };
-  },
+  props: ['products'],
 };
 
 </script>
---
+++ src/data/categories.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    id: 1,
+    title: 'батарейки',
+  },
+  {
+    id: 2,
+    title: 'обои',
+  },
+  {
+    id: 3,
+    title: 'клей',
+  },
+];
