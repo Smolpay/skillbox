71e8cba6144219c94f608268d4d31f14827fc624
Smolpay <Smolpay@gmail.com>
2023-04-20 02:28:28 +0300

    Доработка: Условие для пагинации

--- src/components/BasePagination.vue
+++ src/components/BasePagination.vue
@@ -1,35 +1,35 @@
 <template>
 
   <ul class="catalog__pagination pagination">
-          <li class="pagination__item">
-            <a class="pagination__link pagination__link--arrow"
-               :class="{'pagination__link--disabled' :page <= 1}"
-               aria-label="Предыдущая страница" @click.prevent="paginate(page - 1)">
-              <svg width="8" height="14" fill="currentColor">
-                <use xlink:href="#icon-arrow-left"></use>
-              </svg>
-            </a>
-          </li>
-          <li class="pagination__item" v-for="pageNumber in pages" :key="pageNumber">
-            <a href="#" class="pagination__link"
-               :class="{'pagination__link--current'
+    <li class="pagination__item">
+      <a class="pagination__link pagination__link--arrow"
+         :class="{'pagination__link--disabled' :page <= 1}"
+         aria-label="Предыдущая страница" @click.prevent="paginate(page - 1)">
+        <svg width="8" height="14" fill="currentColor">
+          <use xlink:href="#icon-arrow-left"></use>
+        </svg>
+      </a>
+    </li>
+    <li class="pagination__item" v-for="pageNumber in pages" :key="pageNumber">
+      <a href="#" class="pagination__link"
+         :class="{'pagination__link--current'
                :pageNumber === page}"
-               @click.prevent="paginate(pageNumber)">
-              {{pageNumber}}
-            </a>
-          </li>
-          <li class="pagination__item">
-            <a class="pagination__link pagination__link--arrow"
-               :class="{'pagination__link--disabled' :page >= pages}"
-               href="#"
-               aria-label="Следующая страница" @click.prevent="paginate(page + 1)"
-            >
-              <svg width="8" height="14" fill="currentColor">
-                <use xlink:href="#icon-arrow-right"></use>
-              </svg>
-            </a>
-          </li>
-        </ul>
+         @click.prevent="paginate(pageNumber)">
+        {{ pageNumber }}
+      </a>
+    </li>
+    <li class="pagination__item">
+      <a class="pagination__link pagination__link--arrow"
+         :class="{'pagination__link--disabled' :page >= pages}"
+         href="#"
+         aria-label="Следующая страница" @click.prevent="paginate(page + 1)"
+      >
+        <svg width="8" height="14" fill="currentColor">
+          <use xlink:href="#icon-arrow-right"></use>
+        </svg>
+      </a>
+    </li>
+  </ul>
 </template>
 
 <script>
@@ -47,7 +47,10 @@ export default {
   },
   methods: {
     paginate(page) {
-      this.$emit('paginate', page);
+      if (0 < page && page <= this.pages) {
+        this.$emit('paginate', page);
+      }
+
     },
   },
 
