7ef60b7e04d3bfd6366d69d503082f830b136109
Smolpay <Smolpay@gmail.com>
2023-04-21 16:10:30 +0300

    5.4 Практическая работа Задание 1. «Переключение страниц» Сделана маршрутизация и переключение по страницам (свой маршрутизатор страниц):
    Создано новое свойство, в котором будет храниться текущая страница. А в шаблоне — добавить условия.
    Для каждой страницы сделан отдельный компонент, в котором разметка и логика страницы. В компоненте App оставлены только условия, какой компонент нужно отрисовывать и свойство в состоянии, где будет храниться название текущей страницы.
    Отработан кейс с несуществующей страницей.
    Сделано переключение страницы по нажатию на товар.
    Воспользовался тегом component для отрисовки динамических компонентов.

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,41 @@
 <template>
-  <main class="content container">
-    <div class="content__top content__top--catalog">
-      <h1 class="content__title">
-        Каталог
-      </h1>
-      <span class="content__info">
-        {{ countProducts }} товаров
-      </span>
-    </div>
-    <div class="content__catalog">
-      <ProductFilter :price-from.sync="filterPriceFrom"
-                     :price-to.sync="filterPriceTo"
-                     :category-id.sync="filterCategoryId"
-                     :colors.sync="filterColor"
-      />
-      <section class="catalog">
-        <ProductList :products="products"/>
-        <BasePagination v-model="page" :count="countProducts" :per-page="productsPerPage"/>
-      </section>
-    </div>
-  </main>
+
+  <component :is="currentPageComponent" :page-params="currentPageParams"
+             @goToPage="(pageName, pageParams) => goToPage(pageName, pageParams)"/>
 </template>
 
 <script>
-import products from '@/data/products';
-import ProductFilter from '@/components/ProductFilter';
-import BasePagination from '@/components/BasePagination';
-import ProductList from './components/ProductList.vue';
+import MainPage from '@/pages/MainPage';
+import ProductPage from '@/pages/ProductPage';
+import NotFoundPage from '@/pages/NotFoundPage';
 
+const routes = {
+  main: 'MainPage',
+  product: 'ProductPage'
+};
 export default {
-  name: 'App',
-  components: {
-    ProductList,
-    BasePagination,
-    ProductFilter
-  },
   data() {
     return {
-
-      filterPriceFrom: 0,
-      filterPriceTo: 0,
-      filterCategoryId: 0,
-      filterColor: 0,
-
-      page: 1,
-      productsPerPage: 3,
-
+      currentPage: 'main',
+      currentPageParams: {}
     };
   },
-  computed: {
-
-    filteredProducts() {
-      let filteredProducts = products;
-
-      if (this.filterPriceFrom > 0) {
-        filteredProducts = filteredProducts.filter(product => product.price > this.filterPriceFrom);
-      }
-
-      if (this.filterPriceTo > 0) {
-        filteredProducts = filteredProducts.filter(product => product.price < this.filterPriceTo);
-      }
-
-      if (this.filterCategoryId) {
-        filteredProducts = filteredProducts.filter(product =>
-          product.categoryId === this.filterCategoryId);
-      }
-
-      if (this.filterColor) {
-        filteredProducts = filteredProducts.filter(product => product.color.some(color => color.id === this.filterColor));
-      }
-
-      return filteredProducts;
-    },
-    products() {
-      const offset = (this.page - 1) * this.productsPerPage;
-      return this.filteredProducts.slice(offset, offset + this.productsPerPage);
-    },
-    countProducts() {
-      return this.filteredProducts.length;
-    },
+  methods: {
+    goToPage(pageName, pageParams) {
+      this.currentPage = pageName;
+      this.currentPageParams = pageParams || {};
+    }
   },
+  computed: {
+    currentPageComponent() {
+      return routes[this.currentPage] || 'NotFoundPage';
+    }
+  },
+  components: {
+    MainPage,
+    ProductPage,
+    NotFoundPage,
+  }
 };
 </script>
 <style lang="stylus">
---
+++ src/pages/MainPage.vue
@@ -0,0 +1,89 @@
+<template>
+  <main class="content container">
+    <div class="content__top content__top--catalog">
+      <h1 class="content__title">
+        Каталог
+      </h1>
+      <span class="content__info">
+        {{ countProducts }} товаров
+      </span>
+    </div>
+    <div class="content__catalog">
+      <ProductFilter :price-from.sync="filterPriceFrom"
+                     :price-to.sync="filterPriceTo"
+                     :category-id.sync="filterCategoryId"
+                     :colors.sync="filterColor"
+      />
+      <section class="catalog">
+        <ProductList :products="products" @goToPage="(pageName, pageParams) => $emit('goToPage', pageName, pageParams)"/>
+        <BasePagination v-model="page" :count="countProducts" :per-page="productsPerPage"/>
+      </section>
+    </div>
+  </main>
+</template>
+
+<script>
+import products from '@/data/products';
+import ProductFilter from '@/components/ProductFilter';
+import BasePagination from '@/components/BasePagination';
+import ProductList from '@/components/ProductList.vue';
+
+export default {
+  name: 'MainPage',
+  components: {
+    ProductList,
+    BasePagination,
+    ProductFilter
+  },
+  data() {
+    return {
+
+      filterPriceFrom: 0,
+      filterPriceTo: 0,
+      filterCategoryId: 0,
+      filterColor: 0,
+
+      page: 1,
+      productsPerPage: 3,
+
+    };
+  },
+  computed: {
+
+    filteredProducts() {
+      let filteredProducts = products;
+
+      if (this.filterPriceFrom > 0) {
+        filteredProducts = filteredProducts.filter(product => product.price > this.filterPriceFrom);
+      }
+
+      if (this.filterPriceTo > 0) {
+        filteredProducts = filteredProducts.filter(product => product.price < this.filterPriceTo);
+      }
+
+      if (this.filterCategoryId) {
+        filteredProducts = filteredProducts.filter(product =>
+          product.categoryId === this.filterCategoryId);
+      }
+
+      if (this.filterColor) {
+        filteredProducts = filteredProducts.filter(product => product.color.some(color => color.id === this.filterColor));
+      }
+
+      return filteredProducts;
+    },
+    products() {
+      const offset = (this.page - 1) * this.productsPerPage;
+      return this.filteredProducts.slice(offset, offset + this.productsPerPage);
+    },
+    countProducts() {
+      return this.filteredProducts.length;
+    },
+  },
+};
+
+</script>
+
+<style scoped>
+
+</style>
